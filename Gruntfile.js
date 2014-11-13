8d3c8d6c2cd818736fcbf8c9c171cedc08630dcc
Enzey <enzey@enzey.net>
2014-11-12 22:06:48 -0600

    Update Grunt Template and upload distribution files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,23 @@
 module.exports = function (grunt) {
 
-    var matchdep = require('matchdep'); // dependencies from package
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        distdir: 'dist',
+	var matchdep = require('matchdep'); // dependencies from package
+	var repoLocation = /(https:\/\/github.com)(.*)/.exec(grunt.file.read('.git/config'))[0];
+	var srcdir = 'src';
+	var distdir = 'dist';
+
+	// Get name of folder this file is in.
+	var projectName = /[^\\/]*$/gi.exec(__dirname)[0];
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		distdir: distdir,
+		projectName: projectName,
+		curl: {
+			update_template: {
+				src: 'https://github.com/EnzeyNet/GitHub-Project-Template/archive/master.zip',
+				dest: 'update/master.zip'
+			}
+		},
 		karma: {
 			unit: {
 				configFile: 'karma.conf.js',
@@ -11,48 +25,134 @@ module.exports = function (grunt) {
 				browsers: ['Chrome', 'Firefox']
 			}
 		},
-        concat: {
-            options: {
-                separator: '\n//End of file\n'
-            },
-            dev: {
-                src: [
-                    'src/**/*.js'
-                ],
-                dest: '<%= distdir %>/ng-scrolling-table.js'
-            }
-        },
-        uglify: {
-            production: {
-                files: {
-                    '<%= distdir %>/ng-scrolling-table.min.js': ['src/**/*.js']
-                }
-            }
-        },
-        clean: ["<%= distdir %>"]
-    });
-    matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+		concat: {
+			options: {
+				separator: '\n//End of file\n'
+			},
+			dev: {
+				src: [
+					srcdir + '/**/*.js'
+				],
+				dest: '<%= distdir %>/<%= projectName %>.js'
+			}
+		},
+		uglify: {
+			production: {
+				files: {
+					'<%= distdir %>/<%= projectName %>.min.js': ['<%= distdir %>/<%= projectName %>.js']
+				}
+			}
+		},
+		less: {
+			dev: {
+				options: {
+					cleancss:  false,
+					sourceMap: false,
+					compress:  false,
+					paths: [srcdir + '/less']
+				},
+				files: {
+					'<%= distdir %>/<%= projectName %>.css': srcdir + '/less/<%= projectName %>.less'
+				}
+			},
+			production: {
+				options: {
+					cleancss:  true,
+					sourceMap: true,
+					compress:  true,
+					paths: [srcdir + '/' + projectName + '.less']
+				},
+				files: {
+					'<%= distdir %>/<%= projectName %>.min.css': srcdir + '/less/<%= projectName %>.less'
+				}
+			}
+		},
+		gitclone: {
+			'gh-pages': {
+				options: {
+					branch: 'gh-pages',
+					repository: repoLocation,
+					directory: 'gh-pages'
+				}
+			}
+		},
+		ngAnnotate: {
+			options: {
+				singleQuotes: true,
+			},
+			app1: {
+				files: {
+					'<%= distdir %>/<%= projectName %>.js': ['<%= distdir %>/<%= projectName %>.js']
+				},
+			}
+		},
+		clean: ['<%= distdir %>', 'gh-pages']
+	});
+	matchdep.filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    grunt.registerTask('test', ['karma:unit']);
-    grunt.registerTask('all', ['get-dependencies', 'karma:unit']);
+	grunt.registerTask('get-dependencies', 'Install js packages listed in bower.json', function() {
+		var bower = require('bower');
+		var done = this.async();
 
-    grunt.registerTask('get-dependencies', 'Install js packages listed in bower.json',
-        function() {
-            var bower = require('bower');
-            var done = this.async();
+		bower.commands.install()
+		.on('data', function(data){
+			grunt.log.write(data);
+		})
+		.on('error', function(data){
+			grunt.log.write(data);
+			done(false);
+		})
+		.on('end', function (data) {
+			done();
+		});
+	});
 
-            bower.commands.install()
-            .on('data', function(data){
-                grunt.log.write(data);
-            })
-            .on('error', function(data){
-                grunt.log.write(data);
-                done(false);
-            })
-            .on('end', function (data) {
-                done();
-            });
-        }
-    );
+	grunt.registerTask('all', ['get-dependencies', 'buildDev', 'buildProd', 'test']);
+	grunt.registerTask('buildDev', ['concat', 'less:dev', 'purgeEmptyFiles']);
+	grunt.registerTask('buildProd', ['buildDev', 'ngAnnotate', 'uglify', 'less:production', 'purgeEmptyFiles']);
+	grunt.registerTask('test', ['karma:unit']);
+
+	grunt.registerTask('build-examples', 'Build gh-pages branch of examples.', function() {
+		grunt.task.run('buildDev', 'buildProd', 'update-examples', 'update-examples-dist');
+	});
+
+	grunt.registerTask('purgeEmptyFiles', function() {
+		grunt.file.recurse(distdir, function(abspath, rootdir, subdir, filename) {
+			if (grunt.file.read(abspath).length === 0) {
+				grunt.file.delete(abspath);
+			}
+		});
+	});
+
+	grunt.registerTask('update-examples', function() {
+		if (!grunt.file.exists('gh-pages')) {
+			grunt.task.run('gitclone:gh-pages');
+		}
+		grunt.task.run('update-examples-dist');
+	});
+
+	grunt.registerTask('update-examples-dist', function() {
+		var examplesDist = 'gh-pages/' + distdir + '/';
+		grunt.file.mkdir(examplesDist);
+		grunt.file.recurse(distdir, function (abspath, rootdir, subdir, filename) {
+			grunt.log.error(abspath);
+			grunt.file.copy(abspath, examplesDist + filename);
+		});
+	});
+
+	grunt.registerTask('init-repo', function() {
+		var mainLessFile = srcdir + '/' + 'less' + '/' + projectName + '.less';
+		if (!grunt.file.exists(mainLessFile)) {
+			grunt.file.write(mainLessFile, '');
+		}
+
+		if (!grunt.file.exists(srcdir)) {
+			grunt.file.mkdir(srcdir);
+		}
+
+		if (!grunt.file.exists('gh-pages')) {
+			grunt.task.run('gitclone:gh-pages');
+		}
+	});
 
 };
---
+++ dist/CSSRuleEditor.js
@@ -0,0 +1,238 @@
+(function(angular, CSC) {
+
+	var module = angular.module('net.enzey.service.css.editor', []);
+
+	module.service('nzCssRuleEditor', ['$window', '$http', '$q', function($window, $http, $q) {
+		// Create a new style sheet to hold custom styles
+		angular.element($window.document.head).append( angular.element('<style></style>') );
+
+		// Get last loaded style sheet (the style sheet just added)
+		var styleSheet = $window.document.styleSheets[document.styleSheets.length - 1];
+
+		//var cssRuleCode = $window.document.all ? 'rules' : 'cssRules'; //account for IE and FF
+		//var styleSetter = $window.document.all ? 'value' : 'style'; //account for IE and FF
+
+		var CssRuleEditor = this;
+		var cssRuleCache = {};
+
+		var getJsStyleName = function(styleName) {
+			var firstCharacterRegex = new RegExp('^.');
+			styleName = styleName.split('-');
+			for (var i = 1; i < styleName.length; i++) {
+				styleName[i] = styleName[i].replace(firstCharacterRegex, styleName[i][0].toUpperCase());
+			}
+
+			return styleName.join('');
+		};
+
+		var getStyles = function(cssRule) {
+			var styles;
+			if (cssRule['value']) {
+				styles = cssRule['value'];
+			} else {
+				styles = cssRule['style'];
+			}
+			return styles;
+		};
+
+		var getCssRules = function(styleSheet) {
+			var cssRules;
+			if (styleSheet['rules']) {
+				cssRules = styleSheet['rules'];
+			} else {
+				cssRules = styleSheet['cssRules'];
+			}
+			return cssRules;
+		};
+
+		var extractStyles = function(styles) {
+			var extractedStyles = {};
+			for (var i = 0; i < styles.length; i++) {
+				var styleName = getJsStyleName(styles[i]);
+				extractedStyles[ styleName ] = styles[ styleName ];
+			}
+			return extractedStyles;
+		};
+
+		var parseStyleSheet = function(styleSheet) {
+			var cssRules = getCssRules(styleSheet);
+
+			for (var iRule = 0; iRule < cssRules.length; iRule++) {
+				var cssRule = cssRules[iRule];
+				if (cssRule instanceof CSSStyleRule) {
+					var styles = getStyles(cssRule);
+					styles = extractStyles(styles);
+					cssRule.selectorText.split(',').forEach(function(selector) {
+						selector = selector.replace(new RegExp('^ *'), '');
+						selector = selector.replace(new RegExp(' *$'), '');
+
+						if (!allCssRules[selector]) {
+							allCssRules[selector] = {};
+						}
+						angular.extend(allCssRules[selector], styles);
+					});
+				}
+			};
+		};
+
+		var cacheStyleSheets = function() {
+			var allStyleSheets = $window.document.styleSheets;
+			var styleSheetCache = new Array(allStyleSheets.length-1);
+
+			var promises = [];
+			for (var i = 0; i < allStyleSheets.length-1; i++) {
+				(function () {
+					var index = i;
+					var currentStyleSheet = allStyleSheets[i];
+					var deferred = $q.defer();
+					promises.push(deferred.promise);
+
+					try {
+						getCssRules(currentStyleSheet);
+						styleSheetCache[index] = currentStyleSheet;
+						deferred.resolve(currentStyleSheet);
+					} catch (e) {
+						$http.get(currentStyleSheet.href).success(function(data, status, headers, config) {
+							var tempStyle = angular.element("<style></style>");
+							tempStyle.html(data);
+							angular.element($window.document.head).prepend(tempStyle);
+
+							var allStyleSheets = $window.document.styleSheets;
+							var lastStyleSheet = allStyleSheets[0];
+
+							styleSheetCache[index] = lastStyleSheet;
+
+							tempStyle.remove();
+							deferred.resolve(lastStyleSheet);
+						}).error(function(data, status, headers, config) {
+							deferred.reject();
+						});
+					}
+				})()
+			}
+			var allPromises = $q.all(promises).then(function(foo, bar) {
+				return styleSheetCache;
+			});
+
+			return allPromises;
+		};
+
+		var allCssRules = {};
+		var baseStyleSheets = [];
+
+		this.getBaseRules = function() {
+			var promise = cacheStyleSheets().then(function(styleSheetCache) {
+				baseStyleSheets = styleSheetCache;
+				styleSheetCache.forEach(function(styleSheet) {
+					parseStyleSheet(styleSheet);
+				});
+				return angular.extend({}, allCssRules);
+			});
+
+			return promise;
+		};
+
+		this.getCustomRule = function(ruleName) {
+			if (!ruleName) return;
+
+			var cssRule = cssRuleCache[ruleName];
+			if (!cssRule) {
+				// Rules does not exist
+				styleSheet.insertRule(ruleName + ' {}', 0);
+
+				var cssRules = getCssRules(styleSheet);
+				cssRuleCache[ruleName] = cssRules[0];
+				//cssRuleCache[ruleName] = styleSheet[cssRuleCode][0];
+				cssRule = cssRuleCache[ruleName];
+			}
+
+			return getStyles(cssRule);
+			//return cssRule[styleSetter];
+		};
+
+		this.removeCustomRule = function(ruleName) {
+			if (!ruleName) return;
+
+			var cssRules = getCssRules(styleSheet);
+			for (var i = 0; i < cssRules.length; i++) {
+				var cssRule = cssRules[i];
+				if (cssRule.selectorText.toLowerCase() === ruleName.toLowerCase()) {
+					styleSheet.deleteRule(i);
+					cssRuleCache[ruleName] = null;
+					delete cssRuleCache[ruleName];
+				}
+			}
+		};
+
+		this.getAllCustomRules = function() {
+			return angular.extend({}, cssRuleCache);
+		};
+
+		this.removeAllCustomRules = function() {
+			Object.keys(cssRuleCache).forEach(function (ruleName){
+				CssRuleEditor.removeCustomRule(ruleName);
+			});
+		};
+
+		var createNumberFromSpecificity = function(specificity) {
+			specificity[1] << 20 | specificity[2] << 10 | specificity[3];
+		};
+		var ruleSspecificityCache = {};
+		var getSpecificity = function(rule) {
+			var result = ruleSspecificityCache[rule];
+			if (!result) {
+				var specificity = CSC.calculate(rule)[0].specificity.split(',');
+				ruleSspecificityCache[rule] = createNumberFromSpecificity(specificity);
+				result = ruleSspecificityCache[rule];
+			}
+
+			return result;
+		};
+
+		this.getStyles = function(element) {
+			var deferred = $q.defer();
+
+			var collectRules = function(rule) {
+				var foundElements = $window.document.querySelectorAll(rule);
+				for (var i = 0; i < foundElements.length; i++) {
+					if (element === foundElements[i]) {
+						return rule;
+					}
+				}
+			};
+			CssRuleEditor.getBaseRules().then(function(baseRules) {
+				var validStyles = [];
+				Object.keys(baseRules).map(collectRules).forEach(function(rule) {
+					if (!rule) {return;}
+					validStyles.push({
+						rule: rule,
+						styles: baseRules[rule],
+					});
+				});
+				var customRules = CssRuleEditor.getAllCustomRules();
+				Object.keys(customRules).map(collectRules).forEach(function(rule) {
+					if (!rule) {return;}
+					validStyles.push({
+						rule: rule,
+						styles: extractStyles( getStyles(customRules[rule]) ),
+					});
+				});
+
+				validStyles.sort(function(a, b){
+					return getSpecificity(a.rule) - getSpecificity(b.rule);
+				});
+
+				var stylesForElement = {};
+				validStyles.forEach(function(styleObj) {
+					angular.extend(stylesForElement, styleObj.styles);
+				});
+
+				deferred.resolve(stylesForElement)
+			});
+
+			return deferred.promise;
+		};
+
+	}]);
+
+})(angular, SPECIFICITY);
